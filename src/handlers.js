1583650d521988bfe91ec372e264737c10f85601
moris <moris.rafol@gmail.com>
2020-04-13 16:43:33 +0300

    f: router.js - implemented functionality of the root path

--- src/handlers.js
+++ src/handlers.js
@@ -1,7 +1,30 @@
 
+const url = require("url");
+const axios = require("axios");
+const path = require("path");
+const fs = require("fs") ;
+const hostAndPaths = {
+    search:"http://api.giphy.com/v1/gifs/search",
+    autoComplete:"http://api.giphy.com/v1/gifs/search/tags",
+    suggestions:"api.giphy.com/v1/tags/related/"
+};
+
 
 
 function home(request,response){
+
+    let mainPagePath= path.join(__dirname,"..","public","index.html");
+    fs.readFile(mainPagePath, (error,data)=>{
+        if(error)
+        {
+            badRequestHandler(request,response);
+            return;
+        }
+
+        response.writeHead(200,{"content-type":"text/html"})
+        response.end(data)
+
+    })
 
 
 }
@@ -13,25 +36,40 @@ function resources(request,response){
 }
 
 
-function getSuggestions(request,response){
+function getAutoComplete(request,response){
+
 
     // call fetchSuggestionsFromApi()
 }
 
-
-/***
- *
- * @param searchStr - the search str
- * @return array of strings - options
- */
-function fetchSuggestionsFromApi(searchStr){
+function fetchAutoCompleteFromApi(searchStr){
+    let urlObj = new url.URL(hostAndPaths.autoComplete);
+    urlObj.searchParams.set("api_key","ZrUrI0GTfFYUKWIV78zDckNWUQ2DLfBo");
+    urlObj.searchParams.set("q",searchStr);
 
 
-    //returns array with options ["", "...", etc...]
+    axios.get(urlObj.toString())
+        .then(response=> {
+            let suggestionsArr = Array.from(response.data.data).map(sug=> sug.name);
+            console.log(suggestionsArr);
+            return suggestionsArr;
+        })
+        .catch(e=> console.log(e));
+
+}
+
+
+function notFoundHandler(request,response){
+
+}
+
+function badRequestHandler(request,response){
+//error 400
 }
 
 
 
+fetchAutoCompleteFromApi("cat");
 
 
-module.exports = {home,resources,getSuggestions}
+module.exports = {home,resources,getAutoComplete}
--- src/router.js
+++ src/router.js
@@ -1,10 +1,23 @@
 
-
+const url = require("url");
+const path = require("path");
 const handlers = require("./handlers");
 
 
-module.exports = (request,response) =>{
+function router (request,response) {
+    let pathname = url.parse(request.url).pathname;
 
 
+    if(pathname === "/")
+        handlers.home(request,response);
 
-};
+    else if(pathname.split(".").length === 2)
+        handlers.resources(request,response);
+
+
+    else if(pathname === "/search" && request.method === "GET")
+        handlers.getAutoComplete(request,response);
+
+
+}
+module.exports = router;
