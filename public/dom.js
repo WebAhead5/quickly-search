5825afd790e51422cc37ec9c09eec8a52d229f0d
moris <moris.rafol@gmail.com>
2020-04-14 20:33:05 +0300

    feat: dom.js - loads suggestions, search auto-complete and content into the dom

--- public/dom.js
+++ public/dom.js
@@ -1,40 +1,84 @@
-function displayData(){
-    var contents = document.getElementById('contentContainer');
-    var input = document.getElementById('searchInput');
-    var searchBtn = document.getElementById('searchButton');
-    
-    var data;
-    
-    //on click search button
-    searchBtn.addEventListener('click', () => {
-        getSearch({q: input.value}, (err,resp) => {
-            data = resp; 
-            console.log(data);
-        });
+
+
+var contents = document.getElementById('contentContainer');
+var suggestionsContainer = document.getElementById('suggestionsContainer');
+var autoCompleteContainer = document.getElementById('autocompleteContainer');
+var input = document.getElementById('searchInput');
+var searchBtn = document.getElementById('searchButton');
+
+
+//on click search button
+searchBtn.addEventListener('click', () => {
+    loadData(input.value);
+});
+//if mousedown is the return button
+input.addEventListener('input', () => {
+    let str =input.value;
+    loadData(str)
+
+});
+
+input.addEventListener('change', () => {
+
+
+});
+
+
+
+function loadData(str){
+    //hide container if empty
+    contents.classList.toggle("hidden",!str || str ==="")
+
+
+    getSearch({q: str}, (err,resp) => {
+        contents.innerHTML="";
+        loadContentIntoHtml(resp,contents)
     });
-    
-    //if mousedown is the return button
-    input.addEventListener('input', () => {
-            getSearch({q: input.value}, (err,resp) => {
-            data = resp;
-            contents.innerHTML="";
-            loadData(resp,contents)
-        });
+
+    getSuggestions({q: str}, (err,resp) => {
+        suggestionsContainer.innerHTML="";
+        loadSuggestionsIntoHtml(resp,suggestionsContainer)
     });
-                      
+
+    getAutocomplete({q:str},(err,resp)=> {
+        autoCompleteContainer.innerHTML="";
+        loadAutocompleteIntoHtml(resp,autoCompleteContainer)
+    });
 
 }
-displayData();
 
-function loadData(dataToLoad,container){
 
+function loadContentIntoHtml(dataToLoad, container){
 
     dataToLoad.forEach(obj => {
-        console.log(obj)
 
        let gify = document.createElement('div');
-        gify.innerHTML= `<img src="${obj.original.url}">`;
+        gify.innerHTML= `<img src="${obj["preview_gif"].url}">`;
         gify.classList.add("contentElement")
         container.appendChild(gify);
+    });
+}
+function loadSuggestionsIntoHtml(dataToLoad, container){
+    dataToLoad.forEach(obj => {
+
+        let bubble = document.createElement('span');
+        bubble.textContent= obj;
+        bubble.onclick = ()=>{
+            input.value= obj;
+            loadData(obj)
+
+        };
+
+        bubble.classList.add("suggestionBubble");
+        container.appendChild(bubble);
+    });
+}
+function loadAutocompleteIntoHtml(dataToLoad, container){
+    dataToLoad.forEach(obj => {
+
+        let option = document.createElement('option');
+        option.value= obj;
+
+        container.appendChild(option);
     });
 }
