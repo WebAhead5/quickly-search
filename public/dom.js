83ae9070326f833b3201860b15b67a4c3664e73c
khaled <thenonexist@hotmail.com>
2020-04-14 17:46:41 +0300

    pull request

--- public/dom.js
+++ public/dom.js
@@ -1,22 +1,61 @@
-
-
-async function displayData(){
-    try{
-        const response = await fetch('/router');
-        const data = await response.json(); 
-
-        var contents = document.getElementById('container');
-        var gify;
-        data.forEach(obj => {
-            gify = document.createElement('img');
-            // gify location: obj.data[0].images.original.url
-            gify.src =`${obj.images.original.url}`;
-            gify.alt = `${obj.title}`;
-            gify.width = "250";
-            gify.height = "250";
-            contents.appendChild(gify);
+function displayData(){
+    
+    var contents = document.getElementById('container');
+    var searchInput= document.getElementById('search');
+    var data;
+    
+    //on click search button
+    SearchButton.addEventListener('click', () => {
+        getSearch({q: searchInput.value}, (err,resp) => {
+            data = resp; 
+            console.log(data);
         });
-    } catch(err) {
-        alert(err);
-    } 
+    });
+    
+    //if mousedown is the return button
+    searchInput.addEventListener('input', () => {
+            getSearch({q: searchInput.value}, (err,resp) => {
+            data = resp; 
+            console.log(data);
+        });
+    });
+                      
+    
+    var gify;   
+    data.forEach(obj => {
+        gify = document.createElement('img');
+        gify.src =`${obj.original.url}`;
+        gify.alt = `${obj.title}`;
+        gify.width = `${obj.original.height}`;
+        gify.height = `${obj.original.width}`;
+        contents.appendChild(gify);
+    });
 }
+
+
+
+//function displayData(){
+//    var contents = document.getElementById('container');
+//    var gify;   
+//    fetchData()
+//        .then ( data => { 
+//            data.forEach(obj => {
+//            gify = document.createElement('img');
+//            gify.src =`${obj.original.url}`;
+//            gify.alt = `${obj.title}`;
+//            gify.width = `${obj.original.height}`;
+//            gify.height = `${obj.original.width}`;
+//            contents.appendChild(gify);
+//            })
+//    })
+//        .catch( err => {
+//            alert('display error results');
+//    });
+//}
+//
+//
+//async function fetchData(){
+//        var str= document.getElementById('search').value;
+//        const response = await fetch('/search?q=' + encodeURI("search str"));
+//        const data = await response.json();    
+//}
