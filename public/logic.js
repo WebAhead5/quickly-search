aa3ceddcbfedd771be9c9fd5075a60e37acb5510
moris <moris.rafol@gmail.com>
2020-04-15 20:14:21 +0300

    feat: adds comments and documentation

--- public/logic.js
+++ public/logic.js
@@ -4,29 +4,35 @@ const logic = {
 
     getAutocomplete:
         function (params, cb) {
-            logic.fetchFromAPI(params, '/autocomplete', cb)
+            logic.fetchJsonFromBackend(params, '/autocomplete', cb)
         },
     getSuggestions:
         function (params, cb) {
-            logic.fetchFromAPI(params, '/suggestions', cb)
+            logic.fetchJsonFromBackend(params, '/suggestions', cb)
         },
     getSearch:
         function (params, cb) {
-            logic.fetchFromAPI(params, '/search', cb)
+            logic.fetchJsonFromBackend(params, '/search', cb)
         },
     getTrending:
         function (params, cb) {
-            logic.fetchFromAPI(params, '/trending', cb)
+            logic.fetchJsonFromBackend(params, '/trending', cb)
         },
     getWallpaper:
         function (params, cb) {
-            logic.fetchFromAPI(params, '/wallpaper', cb)
+            logic.fetchJsonFromBackend(params, '/wallpaper', cb)
         },
-    fetchFromAPI:
-        function (params, path, cb) {
+    fetchJsonFromBackend:
+        /***
+         * makes a get request to the provided url with params, and calls the callback on result/error.
+         * @param {object} params the params to add to the url-path
+         * @param {string} localURL - the local path-url for the get request
+         * @param {function(error:object, jsonResult: object)} cb - a callback function
+         */
+        function (params, localURL, cb) {
             let queryParams = Object.keys(params).map((p) => `${p}=${encodeURI(params[p].toString())}`);
             queryParams = queryParams.join('&');
-            let urlWithParams = `${path}${Object.keys(params).length === 0 ? "" : "?"}${(queryParams)}`;
+            let urlWithParams = `${localURL}${Object.keys(params).length === 0 ? "" : "?"}${(queryParams)}`;
             fetch(urlWithParams)
                 .then(res => res.json())
                 .then(jsonObj => cb(null, jsonObj))
@@ -35,6 +41,12 @@ const logic = {
 
 
     onScrollerAt:
+        /***
+         * calls the callback the the user passes a certain percentage of it.
+         * @param {HTMLElement} scrollingDiv - the div with a scroll bar
+         * @param {number} posPercentage - a number between 0 and 1
+         * @param {function()} cb
+         */
         function (scrollingDiv, posPercentage, cb) {
 
             // document bottom
@@ -48,12 +60,24 @@ const logic = {
 
         },
     runOnceDelay:
+        /***
+         * wraps a callback function with a setTimeout, and clears the timeout with the provided ID object (timeoutID).
+         * @param {object} timeoutID - a timeoutObject to clear/cancel
+         * @param {number} timeoutMS - the delay of the timeout in milliseconds
+         * @param {function()}cb - the callback function to call after the delay is over
+         * @return {Object} an object of type timeoutObject
+         */
         function (timeoutID, timeoutMS, cb) {
 
             clearTimeout(timeoutID);
             return setTimeout(cb, timeoutMS)
         },
     isScrollable:
+        /***
+         * returns true of the div can scroll (holds overflowing items).
+         * @param div the html element to check
+         * @return {boolean}
+         */
         function (div) {
             let height = div.clientHeight;
             let scrollableAreaSize = div.scrollHeight;
